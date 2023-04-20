6d004f561b14375d563cafa7ab8bc5d80f996098
luca <lucalorenzi.fi@gmail.com>
2023-04-20 20:18:21 +0200

    table and colors
    added earthquakes table inside the sidebar and different color for each magnitude

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let results = [];
 
+
 fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", 
 {
     "method": "GET",
@@ -9,37 +10,21 @@ fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
 .catch(error => console.log("Si è verificato un errore!: " + error));
 
 function displayMap(response) {
-    results = response.features;   
+    results = response.features;
+    console.log(results);   
     // genera la mappa settando latitudine, longitudine e zoom
     let map = L.map('map').setView([results[0].geometry.coordinates[1], results[0].geometry.coordinates[0]], 6);
 
     // aggiunge il layer di OpenStreetMap alla mappa creata (rende la mappa visibile al client in PNG)
     L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     }).addTo(map);
 
     // create the sidebar instance and add it to the map
         var sidebar = L.control.sidebar({ container: 'sidebar' })
         .addTo(map)
         .open('home');
-
-    // add panels dynamically to the sidebar
-    sidebar
-        .addPanel({
-            id:   'js-api',
-            tab:  '<i class="fa fa-gear"></i>',
-            title: 'JS API',
-            pane: '<p>The Javascript API allows to dynamically create or modify the panel state.<p/><p><button onclick="sidebar.enablePanel(\'mail\')">enable mails panel</button><button onclick="sidebar.disablePanel(\'mail\')">disable mails panel</button></p><p><button onclick="addUser()">add user</button></b>',
-        })
-        // add a tab with a click callback, initially disabled
-        .addPanel({
-            id:   'mail',
-            tab:  '<i class="fa fa-envelope"></i>',
-            title: 'Messages',
-            button: function() { alert('opened via JS callback') },
-            disabled: true,
-        })
 
     // be notified when a panel is opened
     sidebar.on('content', function (ev) {
@@ -52,16 +37,6 @@ function displayMap(response) {
         }
     });
 
-    var userid = 0
-    function addUser() {
-        sidebar.addPanel({
-            id:   'user' + userid++,
-            tab:  '<i class="fa fa-user"></i>',
-            title: 'User Profile ' + userid,
-            pane: '<p>user ipsum dolor sit amet</p>',
-        });
-    }
-
     for (let i = 0; i < results.length; i++) {
 
         let circle = L.circleMarker([results[i].geometry.coordinates[1], results[i].geometry.coordinates[0]], {
@@ -72,8 +47,50 @@ function displayMap(response) {
             weight: 0.7
         }).addTo(map);
 
+        if (results[i].properties["mag"] < 1) {
+            circle.setStyle({
+                fillColor: 'grey'
+            })
+        } else if (results[i].properties["mag"] >= 1 && results[i].properties["mag"] < 2) {
+            circle.setStyle({
+                fillColor: 'lightblue'
+            })
+        } else if (results[i].properties["mag"] >= 2 && results[i].properties["mag"] < 4) {
+            circle.setStyle({
+                fillColor: 'yellow'
+            })
+        } else if (results[i].properties["mag"] >= 4 && results[i].properties["mag"] < 5) {
+            circle.setStyle({
+                fillColor: 'orange'
+            })
+        } else if (results[i].properties["mag"] >= 5 && results[i].properties["mag"] < 6) {
+            circle.setStyle({
+                fillColor: 'red'
+            })
+        } else if (results[i].properties["mag"] >= 6) {
+            circle.setStyle({
+                fillColor: 'darkred'
+            })
+        }
+
         let popupText = "<b>" + results[i].properties["title"] + "</b>";
-        let popupLink = "<b> <a style='width:100%;text-align:center;' target='_blank' href='http://www.cnn.com'>Click here for details</a> </b>";
+        let popupLink = "<b> <a target='_blank' href='http://www.cnn.com'>Click here for details</a> </b>";
         circle.bindPopup(popupText + "<br>" + popupLink);
+    }
+
+    let table_body = document.getElementById("table");
+
+    for (let i = 0; i < results.length; i++) {
+    
+    let data = new Date(results[i].properties["time"]);
+    let tr = document.createElement('tr');
+
+    tr.innerHTML = `     
+        <td> <a style="color:blue" class="move-to">${results[i].properties["place"]}</a> </td>
+        <td>${data}</td>  
+        <td>${((results[i].properties["mag"]).toFixed(2))} </td>
+    `;
+
+        table_body.appendChild(tr);
     }
 }

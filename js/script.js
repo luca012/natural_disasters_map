d4f7b4426e16c765dcaeb7e72cdcf07f03ee6e7f
luca <lucalorenzi.fi@gmail.com>
2023-04-21 11:38:27 +0200

    finished moveTo function and added icon
    added icon, made references page, and finished moveTo function

--- js/script.js
+++ js/script.js
@@ -9,11 +9,13 @@ fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
 .then(result=>displayMap(result))
 .catch(error => console.log("Si è verificato un errore!: " + error));
 
+let map = L.map('map').setView([0, 0], 6);
+
 function displayMap(response) {
     results = response.features;
     console.log(results);   
     // genera la mappa settando latitudine, longitudine e zoom
-    let map = L.map('map').setView([results[0].geometry.coordinates[1], results[0].geometry.coordinates[0]], 6);
+    
 
     // aggiunge il layer di OpenStreetMap alla mappa creata (rende la mappa visibile al client in PNG)
     L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
@@ -21,12 +23,13 @@ function displayMap(response) {
         attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
     }).addTo(map);
 
-    // create the sidebar instance and add it to the map
+    // crea l'istanza della sidebar e la aggiunge alla mappa
         var sidebar = L.control.sidebar({ container: 'sidebar' })
         .addTo(map)
         .open('home');
 
     // be notified when a panel is opened
+    /*
     sidebar.on('content', function (ev) {
         switch (ev.id) {
             case 'autopan':
@@ -35,8 +38,7 @@ function displayMap(response) {
             default:
             sidebar.options.autopan = false;
         }
-    });
-
+    });*/
     for (let i = 0; i < results.length; i++) {
 
         let circle = L.circleMarker([results[i].geometry.coordinates[1], results[i].geometry.coordinates[0]], {
@@ -85,22 +87,18 @@ function displayMap(response) {
     let data = new Date(results[i].properties["time"]);
     let tr = document.createElement('tr');
 
-    tr.innerHTML = `     
-        <td> <a style="color:blue" class="move-to">${results[i].properties["place"]}</a> </td>
+    tr.innerHTML = `
+        <td> <button class="btn btn-primary" onclick="moveTo(${i})">${results[i].properties["place"]}</button> </td>
         <td>${data}</td>  
         <td>${((results[i].properties["mag"]).toFixed(2))} </td>
     `;
 
         table_body.appendChild(tr);
     }
+}
 
-    let places = document.getElementsByClassName("move-to");
-    
-    function moveTo() {
-        map.setView(new L.LatLng(results[10].geometry.coordinates[1], results[10].geometry.coordinates[0]), 13);
-    }
-
-    for (let i = 0; i < places.length; i++) {
-        places[i].addEventListener("click", moveTo);
-    }
+function moveTo(index) {
+    console.log(index)
+    map.setView(new L.LatLng(results[index].geometry.coordinates[1], results[index].geometry.coordinates[0]), 16, {animation : true});
+    //map.panTo(new L.latLng(results[index].geometry.coordinates[1], results[index].geometry.coordinates[0]), 19);
 }
